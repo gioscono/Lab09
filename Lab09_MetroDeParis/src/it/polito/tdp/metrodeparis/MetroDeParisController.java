edc291bb8a27dec0ea469d7f826a0aef71b66c35
giovanni sconosciuto <giovanni sconosciuto@172.22.57.45>
2017-05-09 16:55:35 +0200

    Creazione interfaccia e inizializzazione

--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
@@ -1,5 +1,61 @@
+/**
+ * Sample Skeleton for 'MetroDeParis.fxml' Controller Class
+ */
+
 package it.polito.tdp.metrodeparis;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.metrodeparis.model.Fermata;
+import it.polito.tdp.metrodeparis.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 public class MetroDeParisController {
-	
+	Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="boxPartenza"
+    private ComboBox<Fermata> boxPartenza; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxArrivo"
+    private ComboBox<Fermata> boxArrivo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnPercorso"
+    private Button btnPercorso; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void doPercorso(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert boxPartenza != null : "fx:id=\"boxPartenza\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert boxArrivo != null : "fx:id=\"boxArrivo\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert btnPercorso != null : "fx:id=\"btnPercorso\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+		List<Fermata> fermate = model.getFermate();
+		boxPartenza.getItems().addAll(fermate);
+		boxArrivo.getItems().addAll(fermate);
+		
+	}
 }
--- Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
+++ Lab09_MetroDeParis/src/it/polito/tdp/metrodeparis/model/Model.java
@@ -1,5 +1,24 @@
 package it.polito.tdp.metrodeparis.model;
 
+import java.util.List;
+
+import it.polito.tdp.metrodeparis.dao.MetroDAO;
+
 public class Model {
+	
+	public void Model(){
+	}
+	
+	private List<Fermata> listaFermate;
+
+	public List<Fermata> getFermate() {
+		if(listaFermate==null){
+			MetroDAO dao = new MetroDAO();
+			listaFermate = dao.getAllFermate();
+		}
+		return listaFermate;
+	}
+	
+	
 
 }
